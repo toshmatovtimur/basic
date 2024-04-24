e86de709d9120f7403144ab1ae5c3e7e8b870052
Тошматов Тимур <ttt10@tpu.ru>
2024-04-24 17:58:32 +0700

    1234

--- config/params.php
+++ config/params.php
@@ -5,6 +5,4 @@ return [
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
     'sol' => '$1f628yIdx',
-	'yandex_recaptcha_site_key' => 'ysc2_fELgpmnmIvfisSKnuNHAEaRRGXEGls8EUe9hVlNAea3eeb7d',
-	'yandex_recaptcha_secret_key' => 'ysc1_fELgpmnmIvfisSKnuNHAH338cg5KVuPSV6wi2Mfzd252c8af',
 ];
